6fc9abcd6d2e962a840e4024017e30839eb77cc8
Breanna12 <56575941+Breanna12@users.noreply.github.com>
2023-07-22 12:28:46 -0700

    commit before changing environment design

--- app/src/main/java/com/example/stemist/challenge_activity2.java
+++ app/src/main/java/com/example/stemist/challenge_activity2.java
@@ -6,6 +6,16 @@ import android.os.Bundle;
 
 public class challenge_activity2 extends AppCompatActivity {
 
+    /* Hydration: 5 points
+     * Caffeine: 8 points
+     * Electronics: 10 points
+     * Snacks: 5 points
+     * Environment: 3 points
+     * Walk: 10 points
+     * Journal: 3 points
+     * Audiobook: 3 points
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
