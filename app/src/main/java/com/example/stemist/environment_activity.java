f6ee5e4c6c1e68d83ef89fa2ca2e92c522310c0c
Breanna12 <56575941+Breanna12@users.noreply.github.com>
2023-07-22 14:10:37 -0700

    added one visible/gone element on environment

--- app/src/main/java/com/example/stemist/environment_activity.java
+++ app/src/main/java/com/example/stemist/environment_activity.java
@@ -3,12 +3,35 @@ package com.example.stemist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 public class environment_activity extends AppCompatActivity {
+
+    TextView Feedback1;
+    CheckBox Q1O1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_environment);
+
+        Feedback1 = (TextView)findViewById(R.id.fb1);
+        Feedback1.setVisibility(View.GONE);
+        Q1O1 = (CheckBox) findViewById(R.id.checkBox2);
+
+        Q1O1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(Q1O1.isChecked()) {
+                    Feedback1.setVisibility(View.VISIBLE);
+                }
+                if(Q1O1.isChecked() == false) {
+                    Feedback1.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
