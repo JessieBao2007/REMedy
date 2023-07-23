0476e2475025e4836fd2c60ae66a63cb1df221ea
JessieBao2007 <jessiebao2007@gmail.com>
2023-07-22 20:24:10 -0700

    added scores

--- app/src/main/java/com/example/stemist/challenge_activity2.java
+++ app/src/main/java/com/example/stemist/challenge_activity2.java
@@ -2,6 +2,9 @@ package com.example.stemist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class challenge_activity2 extends AppCompatActivity {
@@ -17,9 +20,91 @@ public class challenge_activity2 extends AppCompatActivity {
      *
      */
 
+    public int score;
+    TextView displayscore;
+    Button water,caffeine,electro, snack, space, walk, journal, audiobook;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge2);
+
+
+        displayscore=(TextView)findViewById(R.id.testingtexts);
+        water=(Button)findViewById(R.id.waterbtn);
+        caffeine=(Button)findViewById(R.id.caffeinebtn);
+        electro=(Button)findViewById(R.id.digitalbtn);
+        snack=(Button)findViewById(R.id.snackbtn);
+        space=(Button)findViewById(R.id.spacebtn);
+        walk=(Button)findViewById(R.id.walkbtn);
+        journal=(Button)findViewById(R.id.journalbtn);
+        audiobook=(Button)findViewById(R.id.audiobookbtn);
+
+        water.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                score+=5;
+                displayscore.setText(Integer.toString(score));
+            }
+        });
+
+        caffeine.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                score+=8;
+                displayscore.setText(Integer.toString(score));
+            }
+        });
+
+        electro.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                score+=10;
+                displayscore.setText(Integer.toString(score));
+            }
+        });
+
+
+        snack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                score+=5;
+                displayscore.setText(Integer.toString(score));
+            }
+        });
+
+        space.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                score+=3;
+                displayscore.setText(Integer.toString(score));
+            }
+        });
+
+        walk.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                score+=10;
+                displayscore.setText(Integer.toString(score));
+            }
+        });
+
+        journal.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                score+=3;
+                displayscore.setText(Integer.toString(score));
+            }
+        });
+
+        audiobook.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                score+=3;
+                displayscore.setText(Integer.toString(score));
+            }
+        });
+
+
     }
 }
