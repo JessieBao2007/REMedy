aa8bc8a8c01d729a8c8d2395735ac7264b366bce
JessieBao2007 <jessiebao2007@gmail.com>
2023-07-22 19:43:35 -0700

    hello

--- app/src/main/java/com/example/stemist/leaderboard_activity.java
+++ app/src/main/java/com/example/stemist/leaderboard_activity.java
@@ -10,5 +10,6 @@ public class leaderboard_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+
     }
 }
